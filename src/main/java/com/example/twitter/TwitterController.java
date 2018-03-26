c993639f240ef3cc804c43605aeb876727746196
thefk <fredrik.thernelius@arbetsformedlingen.se>
2018-03-26 17:32:57 +0200

    Added Exception handling for IllegalArgumentException

--- src/main/java/com/example/twitter/TwitterController.java
+++ src/main/java/com/example/twitter/TwitterController.java
@@ -1,6 +1,8 @@
 package com.example.twitter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import twitter4j.TwitterException;
 
@@ -15,13 +17,22 @@ public class TwitterController {
     @RequestMapping(value="tag", method = RequestMethod.GET)
     public List<WordItem>  getTwitterTag(@RequestParam("tag") String tag) {
 
-        List<WordItem> words = null;
-        try {
-            words = twitterService.handleRequest(tag);
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
+        throw new IllegalArgumentException("My error message");
+//        List<WordItem> words = null;
+//        try {
+//            words = twitterService.handleRequest(tag);
+//        } catch (TwitterException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return words;
+    }
 
-        return words;
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
+        ErrorResponse error = new ErrorResponse();
+        error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
+        error.setMessage(ex.getMessage());
+        return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
     }
 }

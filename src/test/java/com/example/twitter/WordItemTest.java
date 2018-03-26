50593308c371eba79160bba2dae49ba55ee423a9
thefk <fredrik.thernelius@arbetsformedlingen.se>
2018-03-26 14:33:41 +0200

    updated test

--- src/test/java/com/example/twitter/WordItemTest.java
+++ src/test/java/com/example/twitter/WordItemTest.java
@@ -1,14 +1,10 @@
 package com.example.twitter;
 
-import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 public class WordItemTest {
 
@@ -23,15 +19,17 @@ public class WordItemTest {
     @Test
     public void compareToShouldResultInDecendingOrder() {
         WordItem item1 = new WordItem("justin", 0);
-        WordItem item2 = new WordItem("bieber", 1);
+        WordItem item2 = new WordItem("bieber", 2);
+        WordItem item3 = new WordItem("sweden", 0);
 
-        List<WordItem> items = Arrays.asList(item1, item2);
+        List<WordItem> items = Arrays.asList(item1, item2, item3);
         List<WordItem> sortedItems = items.stream()
                 .sorted()
                 .collect(Collectors.toList());
 
-        assertThat(sortedItems.size()).isEqualTo(2);
+        assertThat(sortedItems.size()).isEqualTo(3);
         assertThat(sortedItems.get(0).getCount()).isGreaterThan(sortedItems.get(1).getCount());
+        assertThat(sortedItems.get(1).getCount()).isEqualTo(sortedItems.get(2).getCount());
 
     }
 }
